6a100281467c3c15cd5a858e60b814206ad2b831
lukescomputers <lrtrujillo@wpi.edu>
2020-02-18 12:11:44 -0500

    i have made some substantial changes

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ struct ArrivalNode {
 		struct ArrivalNode *next;
 		unsigned int arrivalTime;
 		unsigned int thread_id;
-}
+};
 
 
 unsigned int isNinja(int*);
@@ -45,54 +45,133 @@ unsigned int start;
 
 
 struct ArrivalNode *pirateHead;
-struct ArrivalNode *ninjaHead
+struct ArrivalNode *ninjaHead;
+
+
+pthread_mutex_t queueLock;
+pthread_mutex_t *costumeLock;
+
+pthread_cond_t ninjaOnlyCondition = PTHREAD_COND_INITIALIZER;
+
+pthread_mutex_t accessLock;
+
+
+
+void getFitted();
 
 void *arrive(void *vargp) {
 	printf("Arrive() called...\n");
 	
-	while(!start);
-	
-	srand(time(0));
-	
 	int arrival_time = rand() % 24;
+	
+	while(!start);
 	
 	sleep(arrival_time);
 	
+	int *thread_id = (int *) vargp;
 	
-	if(isNinja(((int *) vargp))) {
-			printf("A ninja ");
-	} else {
-			printf("A pirate ");
-	}
+	pthread_mutex_lock(&queueLock);
+
 	
-	printf("arrived at %d hours\n", arrival_time);
 	
-	//queue them here
+		if(isNinja(thread_id)) {
+				printf("A ninja arrived at %d hours\n", arrival_time);
 	
+				
+		} else {
+				printf("A pirate arrived at %d hours\n", arrival_time);
+
+		}
+		
+			
+		pthread_mutex_lock(&accessLock);
+		
+			if(isNinja(thread_id)) {
+				printf("A ninja is being served at %d hours\n", arrival_time);
+	
+					
+			} else {
+					printf("A pirate served at %d hours\n", arrival_time);
+			}
+			
+			getFitted();
+		
+		
+		
+		pthread_mutex_unlock(&accessLock);
+		
+	
+	pthread_mutex_unlock(&queueLock);
 	
 	
 	return NULL;
 }
 
+void getFitted() {
+
+
+
+	for(int x = 0; x < numTeams; x++) {
+		if(pthread_mutex_trylock(&costumeLock[x]) != 0) continue;
+		
+		
+		printf("Fitted by team %d\n", 0) ;
+		//choose the amount of time to be in the room
+		
+		unsigned int time = 2;
+	
+		sleep(time);
+		
+		
+		printf("leaving\n");
+		//now create entry and charge the client
+			
+		pthread_mutex_unlock(&costumeLock[x]);
+		break;
+	}
+}
+
 
 int main(int argc, char** argv) {
+	
+		srand(time(0));
+		
 		numTeams = atoi(argv[1]);
 		numPirates = atoi(argv[2]);
 		numNinjas = atoi(argv[3]);
 		
 		start = 0;
 		
+		costumeLock = malloc(sizeof(pthread_mutex_t) * numTeams);
 		
 		pirates = malloc(sizeof(pthread_t) * numPirates);
 		ninjas = malloc(sizeof(pthread_t) * numPirates);
 		
 		makeThreads();
 		//create the requiste threads
-		
+			
 		pthread_exit(NULL);
+		
+		pthread_mutex_destroy(&queueLock);
+		
+		for(int x = 0; x < numTeams; x++) {
+			pthread_mutex_destroy(&costumeLock[x]);
+		}
+	
 }
 
 void makeThreads() {
+	if(pthread_mutex_init(&queueLock, NULL) != 0) {
+			printf("queueLock failed\n");
+	}
+	
+	for(int x = 0; x < numTeams; x++) {
+		if(pthread_mutex_init(&costumeLock[x], NULL) != 0) {
+			printf("num lock done failed\n");
+		}
+	}
+	pthread_mutex_init(&accessLock, NULL);
+	
 	
 	printf("Making threads..\n");
 	
@@ -120,6 +199,6 @@ void addToQueue(unsigned int thread_id, unsigned int arrivalTime) {
 	//adds to the proper queue, make sure it is ordered by arrival time.
 }
 
-struct ArrivalNode* popHead() {
-		//pops off the head of the queue
-}
+/*struct ArrivalNode* popHead() {
+	
+}*/
